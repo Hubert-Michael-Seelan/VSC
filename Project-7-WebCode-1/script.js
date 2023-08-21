fe2c2adc58a5e063843ae07f9406a253f5b9cf78
Hubert-Michael-Seelan <hubertmichaelseelan@gmail.com>
2023-08-21 16:10:43 +0530

    added new file

--- Project-7-WebCode-1/script.js
+++ Project-7-WebCode-1/script.js
@@ -3,24 +3,43 @@ try {
   const URL = "https://api.openbrewerydb.org/v1/breweries";
   const response = await fetch(URL);
   const result = await response.json();
-  var BreweryData =  document.getElementsByClassName("container container-card")[0];
-  result.forEach(element => {
-    let data = document.createElement("div");
-    data.innerHTML = `<div class="card" style="width: 18rem">
-    <div class="card-body">
-      <h5 class="card-name">${element.name}</h5>
-      <h6 class="card-type mb-2">${element.brewery_type}</h6>
-      <p class="card-address">${element.address_1.city.state_province.postal_code.country}
-      </p>
-      <p class="card-phoneNo">${element.phone}
-      </p>
-      <a href="${element.website_url}" class="card-link">website/a>
-    </div>
-  </div>`;
-    BreweryData.appendChild(data);
-  });
+  printData(result);
 } catch (error) {
   console.log("Error", error);
 }
 }
+function printData(data = {}){
+  renderBrewery(data);
+}
+function renderBrewery(data){
+  const cardsArr = [];
+  const breweryContainer = 
+  document.getElementsByClassName("container card-container")[0];
+  if (data.length > 0){
+    data.forEach(element => {console.log(element);
+      const breweries = `<div class="card" style="width: 18rem; height: 400px; border-radius: 40px">
+      <div class="card-body">
+        <h5 class="card-name">Name: ${element.name}</h5>
+        <h6 class="card-type mb-2">Brewery Type: ${element.brewery_type}</h6>
+        <p class="card-address">Address: ${element.address_1}
+        </p>
+        <p class="card-address">City: ${element.city}
+        </p>
+        <p class="card-address">State: ${element.state_province}
+        </p>
+        <p class="card-address">Zipcode: ${element.postal_code}
+        </p>
+        <p class="card-address">Country: ${element.country}
+        </p>
+        <p class="card-phoneNo">Phone No: ${element.phone}
+        </p>
+        <a href="${element.website_url}" class="card-link">click here for the link</a>
+      </div>
+    </div>`;
+    cardsArr.push(breweries);
+    });
+  }
+  breweryContainer.innerHTML = cardsArr;
+}
+
 getBreweryData();
