8ff695bea85d20f8b6db68a256366183c0db5715
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2023-11-24 18:43:37 +0530

    added

--- Try/script.js
+++ Try/script.js
@@ -1,7 +1,7 @@
-function newSqua(color = ""){
-  const div = document.createElement("div")
-  div.setAttribute("class", "square");
-  div.setAttribute("style",`background: ${color}`);
-  return div;
-}
-document.body.appendChild(newSqua("red"));
+// function newSqua(color = ""){
+//   const div = document.createElement("div")
+//   div.setAttribute("class", "square");
+//   div.setAttribute("style",`background: ${color}`);
+//   return div;
+// }
+// document.body.appendChild(newSqua("red"));
